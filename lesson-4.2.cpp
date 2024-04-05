6e5b3c484a0d79cbddfa1573eac1d4abfdb83df4
Юрий Бояджиев <boyadziev@mail.ru>
2024-04-05 17:31:41 +0300

    Добавьте файлы проекта.

---
+++ lesson-4.2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    int totalDistance = 500;
+    int speed;
+    int time = 5;
+
+    cout << "Москва-Воронеж - " << totalDistance << " км\n\n";
+    cout << "Введите вашу скорость: ";
+    cin >> speed;
+
+    if (speed * time < totalDistance) {
+        cout << "\nВы проехали " << speed * time << "км.\n\n";
+        cout << "Москва-Воронеж — фиг догонишь!\n\n";
+    }
+    else {
+        cout << "\nА догонишь - фиг возьёшь!\n\n";
+    }
+
+    
+
+    return 0;
+}
+
